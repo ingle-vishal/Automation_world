60e5e72b7206a2d59400dcf799e2450dd5d02566
Vishal Ingle <inglevishal360@gmail.com>
2026-05-15 00:50:34 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,11 @@
+# Program to reverse a string
+
+# Take string input from user
+string = input("Enter a string: ")
+
+# Reverse the string
+reversed_string = string[::-1]
+
+# Display the output
+print(f"Original string: {string}")
+print(f"Reversed string: {reversed_string}")
